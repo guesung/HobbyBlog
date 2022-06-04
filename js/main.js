1f426bd3f9de4e8819e7915d8906087dc62dc51a
gueit214 <gueit214@naver.com>
2022-06-04 16:44:13 +0900

    save 6.4

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-const 
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  })
+});
